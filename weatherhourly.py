5730945fd6a94938f9d674a2d750acabee6784ac
elasa7 <elasa7@gmail.com>
2023-04-15 17:22:54 +0200

    fix display day

--- weatherhourly.py
+++ weatherhourly.py
@@ -10,7 +10,7 @@ url = 'https://pogoda.interia.pl/prognoza-szczegolowa-bilgoraj,cId,1496'
 
 # Define a command-line argument for the number of weather entries to display
 parser = argparse.ArgumentParser()
-parser.add_argument("num_entries", nargs='?', default=12, type=int, help="Number of weather entries to display")
+parser.add_argument("num_entries", nargs='?', default=24, type=int, help="Number of weather entries to display")
 args = parser.parse_args()
 
 # Set the text and background colors using ANSI escape sequences
@@ -33,14 +33,15 @@ weather_entries = soup.find_all(class_='weather-entry')[:args.num_entries]
 
 # Loop through each weather entry and extract the desired information
 g=math.floor(args.num_entries/24)
-
+first_run=0
 for  entry in reversed(weather_entries):
     hour = entry.find(class_='hour').get_text()
     temp = entry.find(class_='forecast-temp').get_text()
     condition = entry.find(class_='forecast-phrase').get_text()
 
     # Add the day header to each 24-hour entry
-    if hour == "0":
+    if hour == "0" or first_run == 0:
+        first_run=1
         print(f"{white_on_dark_grey}-------------------------------{reset_colors}")
         print(f"{white_on_dark_grey}Date: {day_header[g].get_text().strip()}{reset_colors}")
         print(f"{white_on_dark_grey}-------------------------------{reset_colors}")
